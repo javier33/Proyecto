74feb0202f7749b5ec99ba86b5d1a1fb2b00ce0a
admin <admin@ARGOS.alamo.local>
2016-12-13 11:30:50 +0100

    prototipo1.0 final

--- demosJV2016/src/JVPrincipal.java
+++ demosJV2016/src/JVPrincipal.java
@@ -24,6 +24,26 @@ public class JVPrincipal {
 	static Usuario[] datosUsuarios = new Usuario[MAX_USUARIOS];
 	static SesionUsuario[] datosSesiones = new SesionUsuario[MAX_SESIONES];
 
+	// Apartado 8:
+	// En este array los 0 indican celdas con células muertas y los 1 vivas.
+	static byte[][] mundo = { 
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //
+			{ 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, //
+			{ 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0 }, //
+			{ 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, //
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 
+			{ 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 }, // 
+			{ 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0 }, //
+			{ 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0 }, // Given:
+			{ 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 1x Planeador
+			{ 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // 1x Flip-Flop
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }  // 1x Still Life
+	};
+
+	static final int TAMAÑO = 12;
+	static final int CICLOS = 120;
+
 	public static void main(String[] args) {	
 		// Apartados 4 y 5: Pruebas previas
 		//probarUsuario();
@@ -32,6 +52,158 @@ public class JVPrincipal {
 		// Apartado 7: 
 		cargarDatosPrueba();		
 		mostrarTodosDatosUsuarios();
+
+		// Apartado 8:
+		if (iniciarSesionCorrecta()) {
+			arrancarSimulacion();
+		}
+		else {
+			System.out.println("\nDemasiados intentos fallidos...");
+		}
+			
+		System.out.println("Fin del programa.");
+	}
+
+	/**
+	 * Apartado 8:
+	 * Controla el acceso de usuario 
+	 * y registro de la sesión correspondiente. 
+	 * @return true si la sesión de usuario es válida.
+	 */
+	private static boolean iniciarSesionCorrecta() {
+		Scanner teclado = new Scanner(System.in);	// Entrada por consola.
+		boolean todoCorrecto = false;				// Control de credenciales de usuario.
+		Usuario usrSesion = null;					// Usuario en sesión.
+		int intentos = 3;							// Contandor de intentos.
+		int sesionesRegistradas = 0;				// Control de sesiones registradas.
+	
+		do {
+			// Pide credencial usuario y contraseña
+			System.out.print("Introduce el nif: ");
+			String credencialUsr = teclado.nextLine();
+			System.out.print("Introduce clave acceso: ");
+			String clave = teclado.nextLine();
+
+			// Buscar usuario coincidente con la credencial
+			for (int i = 0; i < MAX_USUARIOS; i++) {
+				if (datosUsuarios[i].nif.equals(credencialUsr)) {
+					usrSesion = datosUsuarios[i];	// Guarda referencia al usuario encontrado.
+					
+					// Comprobar contraseña.
+					if (usrSesion.claveAcceso.equals(clave)) {
+						todoCorrecto = true;
+						break;						// Termina for de búsqueda.
+					}
+				}
+			}
+			if (todoCorrecto == false) {
+				intentos--;
+				System.out.println("Credenciales incorrectas...");
+				System.out.println("Quedan " + intentos + " intentos... ");
+			}
+		}
+		while (!todoCorrecto && intentos > 0);
+
+		if (todoCorrecto) {
+			// Registra sesión.
+			datosSesiones[sesionesRegistradas] = new SesionUsuario();
+			datosSesiones[sesionesRegistradas].usr = usrSesion;
+			datosSesiones[sesionesRegistradas].fecha = new Date().toString();
+			sesionesRegistradas++; 					// Actualiza contador sesiones.
+			
+			System.out.println("Sesión: " + sesionesRegistradas + '\n' + "Iniciada por: " + usrSesion.nombre + " "
+					+ usrSesion.apellidos);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Apartado 8:
+	 * Ejecuta una simulación del juego de la vida en la consola.
+	 * Utiliza la configuración por defecto.
+	 */
+	static void arrancarSimulacion() {
+		int generacion = 0; 
+		do {
+			System.out.println("\nGeneración: " + generacion);
+			mostrarMundo();
+			mundo = actualizarMundo();
+			generacion++;
+		}
+		while (generacion <= CICLOS);
+	}
+
+	/**
+	 * Apartado 8:
+	 * Despliega en la consola el estado almacenado, corresponde
+	 * a una generación del Juego de la vida.
+	 */
+	private static void mostrarMundo() {
+
+		for (int i = 0; i < TAMAÑO; i++) {
+			for (int j = 0; j < TAMAÑO; j++) {
+				System.out.print((mundo[i][j] == 1) ? "|o" : "| ");
+			}
+			System.out.println("|");
+		}
+	}
+
+	/**
+	 * Apartado 8:
+	 * Actualiza el estado almacenado del Juego de la Vida.
+	 * @return nuevoEstado, el array con los cambios de la siguiente generación.
+	 */
+	private static byte[][] actualizarMundo()  {     					
+
+		byte[][] nuevoEstado = new byte[TAMAÑO][TAMAÑO];
+
+		for (int i = 0; i < TAMAÑO; i++) {
+
+			for (int j = 0; j <= 11; j++) {
+
+				int vecinas = 0;						//células adyacentes
+
+				// las celdas situadas fuera del mundo, con índices fuera de rango, hay que controlarlas
+
+				if (i-1 >= 0)	
+					vecinas += mundo[i-1][j];			//celda N			NO | N | NE
+														//					-----------
+				if (i-1 >= 0 && j+1 < TAMAÑO)			// 					 O |   | E
+					vecinas += mundo[i-1][j+1];			//celda NE			----------- 
+														//					SO | S | SE
+				if (j+1 < TAMAÑO)
+					vecinas += mundo[i][j+1];			//celda E			 
+
+				if (i+1 < TAMAÑO && j+1 < TAMAÑO)
+					vecinas += mundo[i+1][j+1];			//celda SE          
+
+				if (i+1 < TAMAÑO)
+					vecinas += mundo[i+1][j]; 			//celda S           
+
+				if (i+1 < TAMAÑO && j-1 >= 0)
+					vecinas += mundo[i+1][j-1]; 		//celda SO 
+
+				if (j-1 >= 0)
+					vecinas += mundo[i][j-1];			//celda O           			                                     	
+
+				if (i-1 >= 0 && j-1 >= 0)
+					vecinas += mundo[i-1][j-1]; 		//celda NO
+
+				if (vecinas < 2) 
+					nuevoEstado[i][j] = 0; 				// subpoblación, muere
+
+				if (vecinas > 3) 
+					nuevoEstado[i][j] = 0; 				// sobrepoblación, muere
+
+				if (vecinas == 3) 
+					nuevoEstado[i][j] = 1; 				// pasa a estar viva o se mantiene
+
+				if (vecinas == 2 && mundo[i][j] == 1) 						
+					nuevoEstado[i][j] = 1; 				// se mantiene viva
+			}
+		}
+		return nuevoEstado;
 	}
 
 	/**
